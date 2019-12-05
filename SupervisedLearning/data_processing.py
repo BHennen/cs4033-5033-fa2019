0fc0c0f9d5f203aea69914b7661abcc5d93b3406
Bryce Hennen <ecyrbnenneh@gmail.com>
2019-12-05 11:56:21 -0600

    initial working data processing (test until line 92)

--- SupervisedLearning/data_processing.py
+++ SupervisedLearning/data_processing.py
@@ -1,5 +1,7 @@
 import os
 import numpy as np
+from csv import reader
+
 
 class DataProcessor():
     def __init__(self, data_file_path, processed_data_folder, processed_data_filename="processed_data.npz"):
@@ -10,13 +12,13 @@ class DataProcessor():
 
     def check_if_processed(self):
         # Checks for already processed data
-        if os.path.exists(self.processed_data_path):
+        if os.path.exists(self._processed_data_path):
             return True
         else:
             return False
-    
+
     def load_processed_data(self):
-        #Loads already processed data
+        # Loads already processed data
         if self.check_if_processed():
             with np.load(self._processed_data_path) as data:
                 self.test_X = data['test_X']
@@ -27,7 +29,7 @@ class DataProcessor():
                 self.validation_y = data['validation_y']
         else:
             raise FileNotFoundError(f"Data not processed yet; no file found at:{self.processed_data_path}")
-                
+
     def process_data(self, splits):
         '''
         splits: 3-tuple of floats which adds to 1, which is the proportion of data that will be allocated to
@@ -37,26 +39,68 @@ class DataProcessor():
         if sum(splits) != 1:
             raise ArithmeticError("process_data: splits does not add to 1")
 
-        #Process data for the first time
-        if os.path.exists(self.data_file_path):
-            #Process data
-            ##TODO: update function to match format of data file
-            data = np.genfromtxt(self._data_file_path)
-            #TODO: We might have to make our own predictor columns
+        # Process data for the first time
+        if os.path.exists(self._data_file_path):
+            # Process data
 
-            #Shuffle data
+            # Only use certain columns
+            use_cols = (  # 0, #PassengerID
+                        1,  # Survived
+                        2,  # Pclass
+                        # 3, #Name
+                        4,  # Sex
+                        5,  # Age
+                        6,  # SibSp
+                        7,  # Parch
+                        # 8, #Ticket
+                        9,  # Fare
+                        # 10, #Cabin
+                        11,  # Embarked
+            )
+            # Convert certain columns to float values (so we can use numpy arrays)
+            converters = {4: lambda sex: {'male':0.0, 'female':1.0}[sex],
+                          11: lambda embarked: {'S': 0.0, 'C': 1.0, 'Q': 2.0}[embarked]}
+            data = []
+            with open(self._data_file_path) as data_file:
+                for line_no, line in enumerate(reader(data_file)):
+                    if line_no < 1:
+                        continue
+                    # Iterate through columns, skipping those we dont want, and converting others
+                    cols = []
+                    for index, col in enumerate(line):
+                        if index not in use_cols:
+                            continue
+                        if col == '':
+                            # Default value for no data
+                            cols.append(None)
+                        elif index in converters:
+                            # Try checking for converter
+                            cols.append(converters[index](col))
+                        else:
+                            #Try converting to float
+                            try:
+                                cols.append(float(col))
+                            except ValueError as e:
+                                print("Default conversion to float did not work.")
+                                raise e
+                    
+                    data.append(cols)
+            
+            data = np.array(data, dtype=np.float_)
+
+            # Shuffle data
             np.random.shuffle(data)
 
-            #Split into test and validation combined with training data
+            # Split into test and validation combined with training data
             len_test_data = int(len(data) * splits[0])
             test_data = data[0:len_test_data]
             validation_train_data = data[len_test_data:]
-            ## Split remaing into validation and training
+            # Split remaing into validation and training
             len_validation_data = int(len(data) * splits[1])
             validation_data = validation_train_data[0:len_validation_data]
             training_data = validation_train_data[len_validation_data:]
 
-            # TODO: Remove target columns from dataset and save them for later
+            # Remove target columns from dataset and save them for later
             # *_X = only predictors for the data
             # *_y = only target columns for the data
             test_X = np.delete(test_data, 1, 1)  # delete target column
@@ -65,7 +109,7 @@ class DataProcessor():
             training_y = training_data[:, 1]  # Only use target column
             validation_X = np.delete(validation_data, 1, 1)  # delete target column
             validation_y = validation_data[:, 1]  # Only use target column
-            
+
             # Save *_X and *_y data
             self.test_X = test_X
             self.test_y = test_y
@@ -74,10 +118,8 @@ class DataProcessor():
             self.validation_X = validation_X
             self.validation_y = validation_y
 
-            np.savez(self._data_file_path, test_X = test_X, test_y = test_y, training_X = training_X, 
-                     training_y = training_y, validation_X = validation_X, validation_y = validation_y)
+            np.savez(self._data_file_path, test_X=test_X, test_y=test_y, training_X=training_X,
+                     training_y=training_y, validation_X=validation_X, validation_y=validation_y)
 
         else:
-            raise FileNotFoundError(f"No data at path specified:{self.data_file_path}")
-       
-
+            raise FileNotFoundError(f"No data at path specified:{self._data_file_path}")
