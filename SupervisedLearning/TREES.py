d300dbf62a38b9da3b27b352695ab547b20a75ee
Bryce Hennen <ecyrbnenneh@gmail.com>
2019-12-06 20:31:42 -0600

    ran titatic code through random forest and compared with sklearn implementation

--- SupervisedLearning/TREES.py
+++ SupervisedLearning/TREES.py
@@ -5,22 +5,6 @@ from collections import defaultdict
 class RandomForestClassifier():
     ''' A random forest classifier using GINI impurity for the decision tree split criterion.
 
-    ***
-    Pseudocode: (based on Elements of Statistical Learning)
-    For each tree b:
-        From training data, take sample of size N with replacement
-        Until min node size is reached:
-            Select a subset of m features(columns) at random from the p features available
-            Pick the best split-point (the one that produces the most separation between observations
-                in left node vs right node) among the m features.
-            Split the node into two daughter nodes
-    Output the ensemble of trees
-
-    For a classification prediction at a point x:
-        Let Cb(x) be the class prediction of the bth random-forest tree.
-        Then Crf(x) = majority-vote for all Cb(x) in B
-    ***
-
     Skeleton and parameters based on sklearn.ensemble.RandomForestClassifier.
 
     Parameters
